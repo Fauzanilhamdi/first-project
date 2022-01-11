25875e487619afeae9509011488cadeb0aec3fdf
Fauzan24 <93256130+Fauzanilhamdi@users.noreply.github.com>
2022-01-11 07:50:13 +0700

    Add files via upload

---
+++ kuis game.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 10 00:02:59 2022
+
+@author: asus
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  9 16:35:35 2022
+
+@author: asus
+"""
+print("-----------------------------------")
+print("selamat datang di kuis program saya")
+print("-----------------------------------")
+pemain = input("apakah kamu ingin bermain kuis ini??")
+if pemain != "ya":
+    quit()
+print("oke mari kita mulai kuis ini")
+score = 0
+
+pernyataan = input ("apa kepanjangan dari PC? ")
+if pernyataan == "personal computer":
+    print("BENAR")
+    score += 1
+else:
+    print("SALAH")
+    
+pernyataan = input ("apa kepanjangan dari VGA? ")
+if pernyataan == "video graphic array" :
+    print("BENAR")
+    score += 1
+else:
+    print("SALAH")
+    
+pernyataan = input ("apa kepanjangan dari RAM? ")
+if pernyataan == "random access memory" :
+    print("BENAR")
+    score += 1
+else:
+    print("SALAH")
+    
+pernyataan = input ("apa kepanjangan dari HDD? ")
+if pernyataan == "hard disk drive" :
+    print("BENAR")
+    score += 1
+else:
+    print("SALAH")
+
+pernyataan = input ("apa kepanjangan dari USB? ")
+if pernyataan =="universal serial bus" :
+    print("BENAR")
+    score += 1
+else:
+    print("SALAH")
+
+pernyataan = input ("apa kepanjangan dari SSD? ")
+if pernyataan == "solid state drive":
+    print("BENAR")
+    score += 1
+else:
+    print("SALAH")
+
+pernyataan = input ("apa kepanjangan dari LCD? ")
+if pernyataan ==  "liquid crystal display" :
+    print("BENAR")
+    score += 1
+else:
+    print("SALAH")
+
+pernyataan = input("apa kepanjangan dari DVD? ")
+if pernyataan == "digital versaitle disc" :
+    print("BENAR")
+    score += 1
+else:
+    print("SALAH")
+
+pernyataan = input("apa kepanjangan dari VCD? ")
+if pernyataan == "video compact disc":
+    print("Benar")
+    score += 1
+else:
+    print("SALAH")
+    
+pernyataan = input("apa kepanjangan dari LED? ")
+if pernyataan == "light emitting diode":
+    print("BENAR")
+    score += 1
+else:
+    print("SALAH")
+    
+print("anda punya " + str(score)+" pertanyaan yang benar!")
+print("anda mempunyai nilai " + str(score/10 * 100)+"%.")
